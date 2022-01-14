1a6bf67d8d6fd455e1196b569eca8e6fd3420dca
meesmb <60484127+meesmb@users.noreply.github.com>
2022-01-14 11:25:47 +0100

    added some tests

--- src/test/java/AfkoAPI/AbbreviationControllerTest.java
+++ src/test/java/AfkoAPI/AbbreviationControllerTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AbbreviationControllerTests {
+public class AbbreviationControllerTest {
 
     @Autowired
     MockMvc mockMvc;
