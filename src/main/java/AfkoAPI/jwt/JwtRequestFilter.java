8f20e668db8e9342904555b925597ada1d865ee8
s1128571 <s1128571@student.hsleiden.nl>
2022-01-25 17:22:03 +0100

    add filter for role

--- src/main/java/AfkoAPI/jwt/JwtRequestFilter.java
+++ src/main/java/AfkoAPI/jwt/JwtRequestFilter.java
@@ -3,6 +3,7 @@ package AfkoAPI.jwt;
 import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -14,6 +15,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static java.util.Arrays.stream;
 
 // source: https://www.javainuse.com/spring/boot-jwt
 @Component
